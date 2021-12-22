f2e2d8f17cc7360d560112d4df34080f60a472af
Carlos Martinez <curlosmart@gmail.com>
2021-12-21 23:05:08 -0500

    add session cookies and path for user auth

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const cookieSession = require("cookie-session")
 const express = require('express')
 const session = require('express-session');
 const passport = require('passport')
@@ -13,12 +14,16 @@ const usersRouter = require('./routers/users')
 const chatsRouter = require('./routers/chats')
 const authRouter = require('./routers/auth')
 
-app.use(cors())
-app.use(express.json())
+app.use(cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 }))
 
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(cors({
+  origin: 'http://192.168.1.78:8080',
+  methods: 'GET,PUT,POST,DELETE',
+  credentials: true,
+}))
+app.use(express.json())
 
 app.use('/users', usersRouter)
 app.use('/chats', chatsRouter)
--- server/routers/auth.js
+++ server/routers/auth.js
@@ -2,9 +2,35 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 
+const CLIENT_URL = process.env.CLIENT_URL
+
 function isLoggedIn(req, res, next) {
   req.user ? next() : res.sendStatus(401);
 }
+
+router.get("/login/success", (req, res) => {
+  
+  if (req.user) {
+    res.status(200).json({
+      success: true,
+      message: "successful",
+      user: req.user,
+      //   cookies: req.cookies
+    });
+  }
+});
+
+router.get("/login/failure", (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "failure",
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect(CLIENT_URL);
+});
 
 router.get('/google',
   passport.authenticate('google', { scope: [ 'email', 'profile' ] }
@@ -12,25 +38,15 @@ router.get('/google',
 
 router.get( '/google/callback',
   passport.authenticate( 'google', {
-    successRedirect: '/auth/protected',
-    failureRedirect: '/auth/google/failure'
+    successRedirect: CLIENT_URL,
+    failureRedirect: '/auth/login/failure'
   })
 );
-
-router.get('/protected', isLoggedIn, (req, res) => {
-  console.log(req.user)
-  res.send(`Hello ${req.user.displayName}`);
-});
 
 router.get('/logout', (req, res) => {
   req.logout();
   req.session.destroy();
   res.send('Goodbye!');
-});
-
-router.get('/google/failure', (req, res) => {
-  console.log('FUCK')
-  res.send('Failed to authenticate..');
 });
 
 
